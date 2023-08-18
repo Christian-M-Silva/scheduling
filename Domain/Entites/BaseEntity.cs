85bdf8b7ee223e034faa739a7f6368c837a37bcb
Christian-M-Silva <tininjacms@gmail.com>
2023-08-18 07:29:52 -0300

    feat: criando a estrutura do projeto

---
+++ Domain/Entites/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entites
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        private DateTime? _createdAt;
+
+        public DateTime? CreatedAT
+        {
+            get { return _createdAt; }
+            set { _createdAt = value == null ? DateTime.UtcNow : value; }
+        }
+
+        public DateTime? UpdateAt { get; set; }
+    }
+}
---
+++ Domain/Entites/PatientsEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entites
+{
+    public class PatientsEntity : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string MainNumber { get; set; }
+
+        public string OtherNumber { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Adress { get; set; }
+
+        public string ServiceType { get; set; }
+
+        public DateTime ConsultationDate { get; set; }
+    }
+}
